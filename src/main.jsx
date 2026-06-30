e1771f32a87ccc86649c1709c5ff0dda201d9690
Laurent <laurent.a1@live.fr>
2026-06-30 15:30:22 +0200

    Mise en place style CSS global et configuration Routeur

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react'//Mode strict React.
+import { createRoot } from 'react-dom/client'//Fonction d'accrochage HTML.
+import { BrowserRouter } from 'react-router-dom'//Routeur React.
+import App from './App.jsx'//Composant principal.
+import './index.scss'//Style global.
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
