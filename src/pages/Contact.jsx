ef842e9d04ad7de387bb0a89cc543cf7929103b1
Laurent <laurent.a1@live.fr>
2026-07-01 12:38:07 +0200

    Préparation de la page Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,36 @@
-const Contact = () => {//Création composant page contact
+import './Contact.scss'
 
+const Contact = () => {
   return (
-    <section>
-      <h1>Contact</h1>
-      <p>Bienvenue sur mon portfolio de développeur web.</p>
+
+    <section className="contact">
+      <div className="contact_container">
+        
+        <h2>Me contacter</h2>
+
+        <form className="contact_form" action="#" method="POST">
+          
+          <div className="contact_field">{/*field : champ*/}
+            <label htmlFor="nom">Votre nom</label>
+            <input type="text" id="nom" name="nom" required />
+          </div>
+
+          <div className="contact_field">
+            <label htmlFor="email">Votre adresse e-mail</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+
+          <div className="contact_field">
+            <label htmlFor="message">Votre message</label>
+            <textarea id="message" name="message" rows="5" required></textarea>
+          </div>
+
+          <button type="submit" className="contact_bouton">
+            Envoyer le message
+          </button>
+
+        </form>
+      </div>
     </section>
   )
 }
--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -14,7 +14,7 @@ const Projets = () => {
             <h3>Nina Carducci</h3>
             <p>Optimisation complète du référencement naturel (SEO) et mise en conformité avec les normes d'accessibilité (WCAG) d'un site de photographe.</p>
 
-            <a href="LIEN_VERS_TON_REPO_GITHUB_NINA" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/Papa-lo/Nina-Carducci" target="_blank" rel="noopener noreferrer">
               Voir le code sur GitHub
             </a>
           </article>
@@ -23,7 +23,7 @@ const Projets = () => {
             <h3>Mon Vieux Grimoire</h3>
             <p>Développement d'une application de notation de livres avec gestion d'une base de données MongoDB et mise en place de mesures de sécurité en backend.</p>
 
-            <a href="LIEN_VERS_TON_REPO_GITHUB_GRIMOIRE" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/Papa-lo/VieuxGrimoire" target="_blank" rel="noopener noreferrer">
               Voir le code sur GitHub
             </a>
           </article>
